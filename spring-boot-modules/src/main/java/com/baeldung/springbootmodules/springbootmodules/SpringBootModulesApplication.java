7f04f4cc3db06a72f9d9532505a73abe1e8a8619
J. Junior <junior.jaava@gmail.com>
2024-05-20 21:15:50 +0200

    springboot project creation

---
+++ spring-boot-modules/src/main/java/com/baeldung/springbootmodules/springbootmodules/SpringBootModulesApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.springbootmodules.springbootmodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootModulesApplication.class, args);
+	}
+
+}
---
+++ spring-boot-modules/src/test/java/com/baeldung/springbootmodules/springbootmodules/SpringBootModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.springbootmodules.springbootmodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootModulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
