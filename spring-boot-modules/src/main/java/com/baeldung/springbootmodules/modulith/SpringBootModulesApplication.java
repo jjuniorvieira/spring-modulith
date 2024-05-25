97bd53734417f5ded0630acdee619f8a186bdb14
J. Junior <junior.jaava@gmail.com>
2024-05-25 21:08:33 +0200

    rename modulith package

--- spring-boot-modules/src/main/java/com/baeldung/springbootmodules/modulith/SpringBootModulesApplication.java
+++ spring-boot-modules/src/main/java/com/baeldung/springbootmodules/modulith/SpringBootModulesApplication.java
@@ -1,4 +1,4 @@
-package com.baeldung.springbootmodules.springbootmodules;
+package com.baeldung.springbootmodules.modulith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-modules/src/test/java/com/baeldung/springbootmodules/modulith/SpringBootModulesApplicationTests.java
+++ spring-boot-modules/src/test/java/com/baeldung/springbootmodules/modulith/SpringBootModulesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.baeldung.springbootmodules.springbootmodules;
+package com.baeldung.springbootmodules.modulith;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
