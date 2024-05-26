0ef59159f8a2893d88a5a0ff071c65cf5898dd14
J. Junior <junior.jaava@gmail.com>
2024-05-26 18:23:52 +0200

    product service details

--- spring-boot-modules/src/main/java/com/baeldung/springbootmodules/modulith/SpringBootModulesApplication.java
+++ spring-boot-modules/src/main/java/com/baeldung/springbootmodules/modulith/SpringBootModulesApplication.java
@@ -1,5 +1,7 @@
 package com.baeldung.springbootmodules.modulith;
 
+import com.baeldung.springbootmodules.modulith.product.ProductService;
+import com.baeldung.springbootmodules.modulith.product.internal.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootModulesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootModulesApplication.class, args);
+
+		SpringApplication.run(SpringBootModulesApplication.class, args)
+				.getBean(ProductService.class)
+				.create(new Product("jj-training", "good-training", 100));
 	}
 
 }
--- spring-boot-modules/src/main/java/com/baeldung/springbootmodules/modulith/product/ProductService.java
+++ spring-boot-modules/src/main/java/com/baeldung/springbootmodules/modulith/product/ProductService.java
@@ -1,8 +1,8 @@
 package com.baeldung.springbootmodules.modulith.product;
 
 
-//import com.baeldung.modulith.notification.NotificationDTO;
-//import com.baeldung.modulith.notification.NotificationService;
+import com.baeldung.springbootmodules.modulith.notification.NotificationDTO;
+import com.baeldung.springbootmodules.modulith.notification.NotificationService;
 import com.baeldung.springbootmodules.modulith.product.internal.Product;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -13,17 +13,16 @@ import java.util.Date;
 public class ProductService {
 
     private final ApplicationEventPublisher events;
-//    private final NotificationService notificationService;
+    private final NotificationService notificationService;
 
-    public ProductService(ApplicationEventPublisher events
-//            , NotificationService notificationService
+    public ProductService(ApplicationEventPublisher events, NotificationService notificationService
     ) {
         this.events = events;
-//        this.notificationService = notificationService;
+        this.notificationService = notificationService;
     }
 
     public void create(Product product) {
-//        notificationService.createNotification(new NotificationDTO(new Date(), "SMS", product.getName()));
-//        events.publishEvent(new NotificationDTO(new Date(), "SMS", product.getName()));
+        notificationService.createNotification(new NotificationDTO(new Date(), "SMS", product.getName()));
+        events.publishEvent(new NotificationDTO(new Date(), "SMS", product.getName()));
     }
 }
